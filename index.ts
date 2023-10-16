5ae487e6f3a3e9bf4f14af386855457ea1ab2705
Melonisjy <dhy2457@naver.com>
2023-10-16 20:49:38 +0900

    first commit

--- index.ts
+++ index.ts
@@ -3,7 +3,14 @@ import express, { Express, Request, Response } from "express";
 const app: Express = express();
 const port = 3010;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response) => {
+  const body = req.body;
+
+  console.log(body);
+
   return res.json("Hello Expressss!");
 });
 
